d44b5c179a604e28ded3a56ccc30464fb6130883
zohaibkhan47 <154499153+zohaibkhan47@users.noreply.github.com>
2023-12-21 12:39:34 +0500

    Updating vite.config file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/vite-deploy/",
+  build: {
+    rollupOptions: {
+      external: ["/src/main.jsx"],
+    },
+  },
 });
