b778892bab39c9530a43b830135b90325324cc56
zohaibkhan47 <154499153+zohaibkhan47@users.noreply.github.com>
2023-12-21 12:46:01 +0500

    commiting

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/vite-deploy/",
+export default {
   build: {
     rollupOptions: {
-      external: ["/src/main.jsx"],
+      external: ['src/main.jsx'],
     },
   },
-});
+};
